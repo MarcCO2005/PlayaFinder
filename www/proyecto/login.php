6f839f5ac6d2f38c1e2f2213a737eacefabdddbb
Marcos Vallecillos <mavaus@floridauniversitaria.es>
2024-05-17 09:54:21 +0200

    editando codigo

--- www/proyecto/login.php
+++ www/proyecto/login.php
@@ -11,6 +11,7 @@ $loginMessage = $security->doLogin();
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="form/view.css">
+    <link rel="icon" type="image/x-icon" href="img/logo.jpg">
 </head>
 <style>
 
