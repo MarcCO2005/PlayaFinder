9b84992c1ee5286668e64e2d954134c35820544e
Darío López <daloga02@floridauniversitaria.es>
2024-05-20 09:43:58 +0200

    Arreglando errores

--- www/proyecto/class/Mostrar.php
+++ www/proyecto/class/Mostrar.php
@@ -40,7 +40,7 @@ class Mostrar extends Connection{
             $ciudad = $info["ciudad"];
             $cod_post = $info["codigo_postal"];
             $cat = $info["id_categoria"];
-            $cat = $info["Valoracion"];
+            $valoracion = $info["Valoracion"];
 
             $object = new Playa($nombre, $ciudad, $cod_post, $cat, $valoracion);
 
