f404f6acdfd65b969680cf7d86dd1e4fc92040a5
Marc Cuenca Olmos <mcuencaolmos@gmail.com>
2024-05-20 09:35:45 +0200

    Destinos

--- PlayaFinder-main/www/proyecto/prueba3.php
+++ PlayaFinder-main/www/proyecto/prueba3.php
@@ -276,27 +276,7 @@ $info = $security->getUser($email);
                 },
             });
         });
-        $(document).ready(function () {
-            $(".owl-carousel2").owlCarousel({
-                dots: false,
-                loop: true,
-                margin: 50,
-                autoplay: true,
-                autoplayTimeout: 2000,
-                autoplayHoverPause: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    480: {
-                        items: 2,
-                    },
-                    768: {
-                        items: 3,
-                    }
-                },
-            });
-        });
+
     </script>
 </body>
 </html>
